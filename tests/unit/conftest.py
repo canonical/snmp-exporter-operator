65f1a00f85263a8569bcfd97852539825c947ea6
sed-i <82407168+sed-i@users.noreply.github.com>
2025-04-11 15:33:33 -0400

    Fix tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,9 +1,18 @@
 from unittest import mock
 
 import pytest
+from ops.testing import Context
+
+from charm import SNMPExporterCharm
 
 
-@pytest.fixture(autouse=True)
-def mock_snap():
-    with mock.patch("charms.operator_libs_linux.v2.snap.SnapCache") as _fixture:
-        yield _fixture
+@pytest.fixture
+def ctx():
+    with mock.patch("charms.operator_libs_linux.v2.snap.SnapCache"):
+        yield Context(SNMPExporterCharm)
+
+
+# @pytest.fixture(autouse=True)
+# def mock_snap():
+#     with mock.patch("charms.operator_libs_linux.v2.snap.SnapCache") as _fixture:
+#         yield _fixture
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,29 +1,25 @@
 import json
 
-from scenario import Context, Relation, State
-
-import charm
+from ops.testing import Relation, State
 
 
-def test_status_no_config():
-    context = Context(charm_type=charm.SNMPExporterCharm)
+def test_status_no_config(ctx):
     state = State(config={"targets": ""})
-    state_out = context.run(event="config-changed", state=state)
+    state_out = ctx.run(ctx.on.config_changed(), state=state)
     assert state_out.unit_status.name == "blocked"
 
 
-def test_status_with_config():
-    context = Context(charm_type=charm.SNMPExporterCharm)
+def test_status_with_config(ctx):
     state = State(config={"targets": "1.2.3.4"})
-    state_out = context.run(event="config-changed", state=state)
+    state_out = ctx.run(ctx.on.config_changed(), state=state)
     assert state_out.unit_status.name == "active"
 
 
-def test_cos_agent_relation_data_is_set():
+def test_cos_agent_relation_data_is_set(ctx):
     cos_agent_relation = Relation("cos-agent", remote_app_name="grafana-agent")
-    context = Context(charm_type=charm.SNMPExporterCharm)
     state = State(relations=[cos_agent_relation], config={"targets": "1.2.3.4"})
-    state_out = context.run(event=cos_agent_relation.changed_event, state=state)
+    state_out = ctx.run(ctx.on.relation_changed(cos_agent_relation), state=state)
 
-    relation_data = json.loads(state_out.relations[0].local_unit_data["config"])
+    print(state_out)
+    relation_data = json.loads(next(iter(state_out.relations)).local_unit_data["config"])
     assert len(relation_data["metrics_scrape_jobs"]) == 2
