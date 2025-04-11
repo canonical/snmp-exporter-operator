adc2a865bde1e24f474347f608af515d352c306d
sed-i <82407168+sed-i@users.noreply.github.com>
2025-04-11 15:44:31 -0400

    Cleanup

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -10,9 +10,3 @@ from charm import SNMPExporterCharm
 def ctx():
     with mock.patch("charms.operator_libs_linux.v2.snap.SnapCache"):
         yield Context(SNMPExporterCharm)
-
-
-# @pytest.fixture(autouse=True)
-# def mock_snap():
-#     with mock.patch("charms.operator_libs_linux.v2.snap.SnapCache") as _fixture:
-#         yield _fixture
